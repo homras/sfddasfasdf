f83ea70b7f9397794e803f29b45e13e895960b9a
homras <homras.ge@gmail.com>
2026-02-10 12:03:16 +0400

    Update config.js

--- HOMRAS/frontend/config.js
+++ HOMRAS/frontend/config.js
@@ -1,8 +1,9 @@
 const config = {
-    API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:5000/api',
+    API_BASE_URL: process.env.API_BASE_URL || 'https://homras.onrender.com/api',
     APP_NAME: 'HOMRAS',
     DEFAULT_LANGUAGE: 'ka',
     SUPPORTED_LANGUAGES: ['ka', 'en', 'ru']
 };
+
 
 export default config;
